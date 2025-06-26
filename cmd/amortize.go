3a658f11da4ab710b0df8fd91716bcb40da8760b
Ryan Kroon <rykroon.tech@gmail.com>
2025-06-25 20:18:41 -0400

    Added helper methods in internal. Added thousands separator. simplified arguments.

--- cmd/amortize.go
+++ cmd/amortize.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
-	"fmt"
-	"math"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+
+	"github.com/rykroon/ry-cli/internal"
 )
 
 var amortizeCmd = &cobra.Command{
@@ -15,67 +17,53 @@ var amortizeCmd = &cobra.Command{
 	Run:   runAmortizeCmd,
 }
 
-var p float64           // principal
-var homePrice float64   // home price
-var downPercent float64 // down payment as percent
-var i float64           // monthly interest
-var annualRate float64  // annual interest
-var n int               // number of payments
-var years int           // number of years
-var schedule bool
+var p float64
+var rate float64
+var years int
+
+var homePrice float64
+var downPercent float64
+
+var printSchedule bool
 
 func runAmortizeCmd(cmd *cobra.Command, args []string) {
 	if p < 0 {
 		p = homePrice - (homePrice * (downPercent / 100))
 	}
-	if i < 0 {
-		i = annualRate / 12
-	}
-	i = i / 100 // needs to be expressed as a percentage
-	if n < 0 {
-		n = years * 12
-	}
+	r := rate / 12 / 100
+	n := years * 12
 
-	monthlyPayment := p * ((i * math.Pow(1+i, float64(n))) / (math.Pow(1+i, float64(n)) - 1))
+	monthlyPayment := internal.AmortizeLoan(p, r, n)
+	fmt := message.NewPrinter(language.English)
 	fmt.Printf("Monthly Payment: $%.2f\n", monthlyPayment)
 
-	balance := p
+	if printSchedule {
+		payments := internal.AmortizationSchedule(p, r, n)
 
-	if schedule {
 		// header
-		fmt.Printf("%-6s %-12s %-12s %-12s %-12s\n", "Month", "Payment", "Principal", "Interest", "Balance")
+		fmt.Printf("%-6s %-6s %-12s %-12s %-12s %-12s\n", "Year", "Month", "Payment", "Principal", "Interest", "Balance")
 		fmt.Println(strings.Repeat("-", 58))
 
-		for month := 1; month < n; month++ {
-			interestPaid := balance * i
-			principalPaid := monthlyPayment - interestPaid
-			balance -= principalPaid
-			fmt.Printf("%-6d $%-11.2f $%-11.2f $%-11.2f $%-11.2f\n",
-				month, monthlyPayment, principalPaid, interestPaid, balance)
+		for _, payment := range payments {
+			year := payment.Month/12 + 1
+			fmt.Printf("%-6d %-6d $%-11.2f $%-11.2f $%-11.2f $%-11.2f\n",
+				year, payment.Month, monthlyPayment, payment.PrincipalPaid, payment.InterestPaid, payment.Balance)
 		}
 	}
 }
 
 func init() {
 	amortizeCmd.Flags().Float64VarP(&p, "principal", "p", -1, "The loan amount borrowed.")
-	amortizeCmd.Flags().Float64VarP(&i, "monthly-rate", "i", -1, "The monthly interesst rate.")
-	amortizeCmd.Flags().IntVarP(&n, "payments", "n", -1, "Total number of payments.")
+	amortizeCmd.Flags().Float64VarP(&rate, "rate", "r", -1, "Annual interest rate.")
+	amortizeCmd.Flags().IntVarP(&years, "years", "y", 30, "Loan term in years.")
 
 	// Helper Arguments. Mutually exclusive from the above.
 	amortizeCmd.Flags().Float64VarP(&homePrice, "price", "H", -1, "Home purchase price.")
 	amortizeCmd.Flags().Float64VarP(&downPercent, "down-percent", "d", -1, "Down payment as a percent.")
-	amortizeCmd.Flags().Float64VarP(&annualRate, "annual-rate", "r", -1, "Annual interest rate.")
-	amortizeCmd.Flags().IntVarP(&years, "years", "y", -1, "Loan term in years.")
 
 	amortizeCmd.MarkFlagsRequiredTogether("price", "down-percent")
 	amortizeCmd.MarkFlagsMutuallyExclusive("principal", "price")
 	amortizeCmd.MarkFlagsMutuallyExclusive("principal", "down-percent")
 
-	amortizeCmd.MarkFlagsMutuallyExclusive("payments", "years")
-	amortizeCmd.MarkFlagsOneRequired("payments", "years")
-
-	amortizeCmd.MarkFlagsMutuallyExclusive("monthly-rate", "annual-rate")
-	amortizeCmd.MarkFlagsOneRequired("monthly-rate", "annual-rate")
-
-	amortizeCmd.Flags().BoolVarP(&schedule, "schedule", "s", false, "Print the schedule.")
+	amortizeCmd.Flags().BoolVarP(&printSchedule, "schedule", "s", false, "Print the schedule.")
 }
---
+++ internal/amortization.go
@@ -0,0 +1,32 @@
+package internal
+
+import "math"
+
+type Payment struct {
+	Month         int
+	PrincipalPaid float64
+	InterestPaid  float64
+	Balance       float64
+}
+
+func AmortizeLoan(p float64, r float64, n int) float64 {
+	return p * ((r * math.Pow(1+r, float64(n))) / (math.Pow(1+r, float64(n)) - 1))
+}
+
+func AmortizationSchedule(p float64, r float64, n int) []Payment {
+	monthlyPayment := AmortizeLoan(p, r, n)
+	payments := make([]Payment, n+1)
+	balance := p
+	for i := range n {
+		interestPaid := balance * r
+		principalPaid := monthlyPayment - interestPaid
+		balance -= principalPaid
+		payments[i] = Payment{
+			Month:         i + 1,
+			InterestPaid:  interestPaid,
+			PrincipalPaid: principalPaid,
+			Balance:       balance,
+		}
+	}
+	return payments
+}
