4625e0b7cef31683519a7115ef4163e9ba923e69
Ryan Kroon <rykroon.tech@gmail.com>
2025-07-07 19:35:18 -0400

    Added ToMoney() function that returns a decimal as a money string.

--- internal/format/format.go
+++ internal/format/format.go
@@ -1,10 +1,31 @@
 package format
 
 import (
+	"strings"
+
 	"github.com/shopspring/decimal"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 )
+
+func ToMoney(d decimal.Decimal) string {
+	s := d.StringFixed(2)
+	parts := strings.Split(s, ".")
+	if len(parts) != 2 {
+		return ""
+	}
+	lhs := parts[0]
+	i := d.NumDigits()
+	newString := ""
+	for idx, chr := range lhs {
+		if i%3 == 0 && idx != 0 {
+			newString += ","
+		}
+		newString += string(chr)
+		i -= 1
+	}
+	return "$" + newString + "." + parts[1]
+}
 
 func FormatMoney(d decimal.Decimal) string {
 	p := message.NewPrinter(language.English)
