7d3a41c6c28f5394862a720c9d1397ffcde786f1
Ryan Kroon <rykroon.tech@gmail.com>
2025-07-08 19:42:56 -0400

    cleaned up unused functions.

--- internal/format/format.go
+++ internal/format/format.go
@@ -14,31 +14,21 @@ func ToMoney(d decimal.Decimal) string {
 	if len(parts) != 2 {
 		return ""
 	}
-	lhs := parts[0]
+	digitStr := parts[0]
 	i := d.NumDigits()
-	newString := ""
-	for idx, chr := range lhs {
+	moneyString := ""
+	for idx, chr := range digitStr {
 		if i%3 == 0 && idx != 0 {
-			newString += ","
+			moneyString += ","
 		}
-		newString += string(chr)
+		moneyString += string(chr)
 		i -= 1
 	}
-	return "$" + newString + "." + parts[1]
+	return "$" + moneyString + "." + parts[1]
 }
 
 func FormatMoney(d decimal.Decimal) string {
 	p := message.NewPrinter(language.English)
 	f, _ := d.Round(2).Float64()
 	return p.Sprintf("$%.2f", f)
-}
-
-func FormatPercent(d decimal.Decimal) string {
-	s := d.Mul(decimal.NewFromInt(100)).StringFixed(0)
-	return s + "%"
-}
-
-func FormatRate(d decimal.Decimal) string {
-	s := d.Mul(decimal.NewFromInt(100)).StringFixed(2)
-	return s + "%"
 }
