46866bf0d9b1d5f2c577ed8ce6bd821424c959d9
Ryan Kroon <rykroon.tech@gmail.com>
2025-10-06 09:20:00 -0400

    Added tax table for Married Filing Jointly.

--- internal/taxes/taxes.go
+++ internal/taxes/taxes.go
@@ -108,16 +108,29 @@ func (t TaxTable) GetConfig(year int, status FilingStatus) (*FilingConfig, bool)
 }
 
 func buildSingle2025() FilingConfig {
-	single2025 := FilingConfig{}
-	single2025.setStandardDeductionInt(15000)
-	single2025.addBracket(NewBracket(0, 11925, .10))
-	single2025.addBracket(NewBracket(11925, 48475, .12))
-	single2025.addBracket(NewBracket(48475, 103350, .22))
-	single2025.addBracket(NewBracket(103350, 197300, .24))
-	single2025.addBracket(NewBracket(197300, 250525, .32))
-	single2025.addBracket(NewBracket(250525, 626350, .35))
-	single2025.addBracket(NewBracket(626350, 0, .37))
-	return single2025
+	fc := FilingConfig{}
+	fc.setStandardDeductionInt(15000)
+	fc.addBracket(NewBracket(0, 11925, .10))
+	fc.addBracket(NewBracket(11925, 48475, .12))
+	fc.addBracket(NewBracket(48475, 103350, .22))
+	fc.addBracket(NewBracket(103350, 197300, .24))
+	fc.addBracket(NewBracket(197300, 250525, .32))
+	fc.addBracket(NewBracket(250525, 626350, .35))
+	fc.addBracket(NewBracket(626350, 0, .37))
+	return fc
+}
+
+func buildMarriedJointly2025() FilingConfig {
+	fc := FilingConfig{}
+	fc.setStandardDeductionInt(31_500)
+	fc.addBracket(NewBracket(0, 23_850, .10))
+	fc.addBracket(NewBracket(23_850, 96_950, .12))
+	fc.addBracket(NewBracket(96_950, 206_700, .22))
+	fc.addBracket(NewBracket(206_700, 394_600, .24))
+	fc.addBracket(NewBracket(394_600, 501_050, .32))
+	fc.addBracket(NewBracket(501_050, 751_600, .35))
+	fc.addBracket(NewBracket(751_600, 0, .37))
+	return fc
 }
 
 var UsFederalTaxTable = TaxTable{
@@ -125,7 +138,8 @@ var UsFederalTaxTable = TaxTable{
 		2025: {
 			Year: 2025,
 			Filings: map[FilingStatus]FilingConfig{
-				Single: buildSingle2025(),
+				Single:       buildSingle2025(),
+				MarriedJoint: buildMarriedJointly2025(),
 			},
 		},
 	},
