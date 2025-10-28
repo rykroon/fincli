dfe67bdf28692e10444fe4a3b5870c245d672f21
Ryan Kroon <rykroon.tech@gmail.com>
2025-10-28 18:39:47 -0400

    Refactored loading of json.

--- internal/tax/init.go
+++ internal/tax/init.go
@@ -8,24 +8,22 @@ import (
 	"runtime"
 )
 
-const maxIncome uint64 = 1_000_000_000_000_000
-
 var UsFederalRegistry map[uint16]UsTaxSystem
 var FicaRegistry map[uint16]FicaTaxSystem
 var NewJerseyRegistry map[uint16]NjTaxSystem
 
 func init() {
-	// Get the directory of this source file
-	_, filename, _, _ := runtime.Caller(0)
-	dir := filepath.Dir(filename)
-
-	FicaRegistry = loadJson[map[uint16]FicaTaxSystem](filepath.Join(dir, "configs/fica.json"))
-	UsFederalRegistry = loadJson[map[uint16]UsTaxSystem](filepath.Join(dir, "configs/us.json"))
-	NewJerseyRegistry = loadJson[map[uint16]NjTaxSystem](filepath.Join(dir, "configs/nj.json"))
+	FicaRegistry = loadJson[map[uint16]FicaTaxSystem]("fica.json")
+	UsFederalRegistry = loadJson[map[uint16]UsTaxSystem]("us.json")
+	NewJerseyRegistry = loadJson[map[uint16]NjTaxSystem]("nj.json")
 }
 
-func loadJson[T any](filepath string) T {
-	data, err := os.ReadFile(filepath)
+func loadJson[T any](jsonfile string) T {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+	jsonFilePath := filepath.Join(dir, "configs", jsonfile)
+
+	data, err := os.ReadFile(jsonFilePath)
 	if err != nil {
 		panic(fmt.Errorf("failed to read config file: %w", err))
 	}
